3f50e6f37d5fc5d82cbc9ae9863cec61a58ccc11
Lans12345 <olanalans12345@gmail.com>
2023-06-15 18:28:32 +0800

    added home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:agos/screens/tabs/recommended_tab.dart';
+import 'package:agos/screens/tabs/station_list_tab.dart';
 import 'package:agos/utils/colors.dart';
 import 'package:agos/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
@@ -20,140 +20,170 @@ class _HomeScreenState extends State<HomeScreen> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(),
-              height: 220,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: TextRegular(
-                      text: 'Refilling Stations',
-                      fontSize: 24,
-                      color: primary,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 45,
-                        width: 280,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100)),
-                        child: TextFormField(
-                          textCapitalization: TextCapitalization.sentences,
-                          controller: messageController,
-                          decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.search,
-                                color: primary,
-                              ),
-                              suffixIcon: filter != ''
-                                  ? IconButton(
-                                      onPressed: (() {
-                                        setState(() {
-                                          filter = '';
-                                          messageController.clear();
-                                        });
-                                      }),
-                                      icon: const Icon(
-                                        Icons.close_rounded,
-                                        color: primary,
-                                      ),
-                                    )
-                                  : const SizedBox(),
-                              fillColor: Colors.white,
-                              filled: true,
-                              enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    const BorderSide(width: 1, color: primary),
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    const BorderSide(width: 1, color: primary),
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              hintText: 'Search a Station',
-                              border: InputBorder.none,
-                              hintStyle: const TextStyle(
-                                fontFamily: 'QRegular',
-                              )),
-                          onChanged: (value) {
-                            setState(() {
-                              filter = value;
-                            });
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.my_library_books_outlined,
-                          color: primary,
-                          size: 32,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const TabBar(
-                    indicatorSize: TabBarIndicatorSize.label,
-                    indicatorWeight: 1,
-                    labelStyle: TextStyle(
-                      fontFamily: 'QRegular',
-                      color: primary,
-                      fontSize: 14,
-                    ),
-                    unselectedLabelStyle: TextStyle(
-                      fontFamily: 'QRegular',
-                      color: Colors.grey,
-                      fontSize: 12,
-                    ),
-                    labelColor: primary,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      Tab(
-                        text: 'Recommended',
-                      ),
-                      Tab(
-                        text: 'Nearest',
-                      ),
-                      Tab(
-                        text: 'Most rated',
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Expanded(
-              child: SizedBox(
-                child: TabBarView(
+        drawerEnableOpenDragGesture: false,
+        drawer: const Drawer(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 190,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    RecommendedTab(),
-                    SizedBox(),
-                    SizedBox(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Builder(builder: (context) {
+                          return IconButton(
+                            onPressed: () {
+                              Scaffold.of(context).openDrawer();
+                            },
+                            icon: const Icon(
+                              Icons.menu_rounded,
+                              color: primary,
+                            ),
+                          );
+                        }),
+                        TextBold(
+                          text: 'HOME',
+                          fontSize: 24,
+                          color: primary,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.travel_explore_rounded,
+                            color: primary,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100)),
+                          child: TextFormField(
+                            style: const TextStyle(
+                              fontFamily: 'QBold',
+                              color: primary,
+                            ),
+                            textCapitalization: TextCapitalization.words,
+                            controller: messageController,
+                            decoration: InputDecoration(
+                                prefixIcon: const Icon(
+                                  Icons.search,
+                                  color: primary,
+                                ),
+                                suffixIcon: filter != ''
+                                    ? IconButton(
+                                        onPressed: (() {
+                                          setState(() {
+                                            filter = '';
+                                            messageController.clear();
+                                          });
+                                        }),
+                                        icon: const Icon(
+                                          Icons.close_rounded,
+                                          color: primary,
+                                        ),
+                                      )
+                                    : const SizedBox(),
+                                fillColor: Colors.white,
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      width: 1, color: primary),
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                      width: 1, color: primary),
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                hintText: 'Search Stations',
+                                border: InputBorder.none,
+                                hintStyle: const TextStyle(
+                                  fontFamily: 'QRegular',
+                                  color: primary,
+                                  fontSize: 12,
+                                )),
+                            onChanged: (value) {
+                              setState(() {
+                                filter = value;
+                              });
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.my_library_books_outlined,
+                            color: primary,
+                            size: 32,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TabBar(
+                      labelPadding: EdgeInsets.all(0),
+                      indicatorPadding: EdgeInsets.all(0),
+                      indicatorSize: TabBarIndicatorSize.label,
+                      indicatorWeight: 1,
+                      labelStyle: TextStyle(
+                        fontFamily: 'QRegular',
+                        color: primary,
+                        fontSize: 14,
+                      ),
+                      unselectedLabelStyle: TextStyle(
+                        fontFamily: 'QRegular',
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                      labelColor: primary,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(
+                          text: 'Recommended',
+                        ),
+                        Tab(
+                          text: 'Nearest',
+                        ),
+                        Tab(
+                          text: 'Most rated',
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+              const Expanded(
+                child: SizedBox(
+                  child: TabBarView(
+                    children: [
+                      StationList(),
+                      StationList(),
+                      StationList(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/tabs/station_list_tab.dart
@@ -0,0 +1,206 @@
+import 'package:agos/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../widgets/text_widget.dart';
+
+class StationList extends StatelessWidget {
+  const StationList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: 50,
+      separatorBuilder: (context, index) {
+        return const Divider(
+          color: primary,
+        );
+      },
+      itemBuilder: (context, index) {
+        return Padding(
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 175,
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          'assets/images/station.png',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                      color: primary,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                              size: 14,
+                            ),
+                            Icon(
+                              Icons.star_half,
+                              color: Colors.amber,
+                              size: 14,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        TextBold(
+                          text: '4.5',
+                          fontSize: 15,
+                          color: primary,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: Column(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextBold(
+                          text: 'Name of the Station',
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 180,
+                          child: TextRegular(
+                            text:
+                                'Location of the Station Location of the Station Location of the Station Location of the Station',
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        SizedBox(
+                          width: 180,
+                          child: TextRegular(
+                            text: 'Open from: 8am - 7pm',
+                            fontSize: 14,
+                            color: primary,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        TextBold(
+                          text: '2.5km away',
+                          fontSize: 16,
+                          color: primary,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: primary,
+                                    ),
+                                    borderRadius: BorderRadius.circular(100),
+                                  ),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(5.0),
+                                    child: Icon(
+                                      Icons.directions_outlined,
+                                      color: primary,
+                                      size: 16,
+                                    ),
+                                  ),
+                                ),
+                                TextRegular(
+                                  text: 'Directions',
+                                  fontSize: 12,
+                                  color: primary,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          MaterialButton(
+                            elevation: 2,
+                            minWidth: 60,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100)),
+                            color: primary,
+                            onPressed: (() async {}),
+                            child: const Icon(
+                              Icons.send,
+                              color: Colors.white,
+                              size: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
