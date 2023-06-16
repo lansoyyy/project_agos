e008a9f4126a525e2f17c6ca6862ebf8a136f2bd
Lans12345 <olanalans12345@gmail.com>
2023-06-16 18:32:07 +0800

    added forgot password dialog

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -120,7 +120,110 @@ class LoginScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: ((context) {
+                                final formKey = GlobalKey<FormState>();
+                                final TextEditingController emailController =
+                                    TextEditingController();
+
+                                return AlertDialog(
+                                  backgroundColor: primary,
+                                  title: TextRegular(
+                                    text: 'Forgot Password',
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                  ),
+                                  content: Form(
+                                    key: formKey,
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        TextFieldWidget(
+                                          icon: Icons.email,
+                                          hint: 'Email',
+                                          textCapitalization:
+                                              TextCapitalization.none,
+                                          inputType: TextInputType.emailAddress,
+                                          label: 'Email',
+                                          controller: emailController,
+                                          validator: (value) {
+                                            if (value == null ||
+                                                value.isEmpty) {
+                                              return 'Please enter an email address';
+                                            }
+                                            final emailRegex = RegExp(
+                                                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                                            if (!emailRegex.hasMatch(value)) {
+                                              return 'Please enter a valid email address';
+                                            }
+                                            return null;
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: (() {
+                                        Navigator.pop(context);
+                                      }),
+                                      child: TextRegular(
+                                        text: 'Cancel',
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: (() async {
+                                        // if (formKey.currentState!.validate()) {
+                                        //   try {
+                                        //     Navigator.pop(context);
+                                        //     await FirebaseAuth.instance
+                                        //         .sendPasswordResetEmail(
+                                        //             email:
+                                        //                 emailController.text);
+                                        //     showToast(
+                                        //         'Password reset link sent to ${emailController.text}');
+                                        //   } catch (e) {
+                                        //     String errorMessage = '';
+
+                                        //     if (e is FirebaseException) {
+                                        //       switch (e.code) {
+                                        //         case 'invalid-email':
+                                        //           errorMessage =
+                                        //               'The email address is invalid.';
+                                        //           break;
+                                        //         case 'user-not-found':
+                                        //           errorMessage =
+                                        //               'The user associated with the email address is not found.';
+                                        //           break;
+                                        //         default:
+                                        //           errorMessage =
+                                        //               'An error occurred while resetting the password.';
+                                        //       }
+                                        //     } else {
+                                        //       errorMessage =
+                                        //           'An error occurred while resetting the password.';
+                                        //     }
+
+                                        //     showToast(errorMessage);
+                                        //     Navigator.pop(context);
+                                        //   }
+                                        // }
+                                      }),
+                                      child: TextBold(
+                                        text: 'Continue',
+                                        fontSize: 14,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              }),
+                            );
+                          },
                           child: TextRegular(
                               text: 'Forgot Password?',
                               fontSize: 12,
